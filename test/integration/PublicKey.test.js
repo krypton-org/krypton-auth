3817f25e3a2e7e5ae09c74f0cb951872ab0ca58e
JohannC <johann.corcuff@gmail.com>
2019-11-17 21:15:23 +0100

    Tests - Factoring test code

--- test/integration/PublicKey.test.js
+++ test/integration/PublicKey.test.js
@@ -7,7 +7,7 @@ beforeAll((done) => {
     appTester = new AppTester({onReady: done});
 }, 15000);
 
-test('Get service public key', (done) => {
+test('Get service public key', async (done) => {
     request = appTester.getRequestSender();
     const query = {
         query: `query{
@@ -15,15 +15,9 @@ test('Get service public key', (done) => {
               value
             }}`
     }
-    request.get('/graphql')
-        .set('Accept', 'application/json')
-        .set("Content-Type", "application/json")
-        .send(JSON.stringify(query))
-        .end((err, res) => {
-            if (err) { return done(err); }
-            expect(JSON.parse(res.text).data.publicKey.value).toBeTruthy();
-            done();
-        });
+    const res = await request.postGraphQL(query);
+    expect(res.data.publicKey.value).toBeTruthy();
+    done();
 });
 
 
