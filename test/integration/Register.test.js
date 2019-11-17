b05569c9626a149b403488ed95e120e6b1187042
JohannC <johann.corcuff@gmail.com>
2019-11-17 20:59:21 +0100

    Tests - New test cases for registration errors

--- test/integration/Register.test.js
+++ test/integration/Register.test.js
@@ -89,6 +89,90 @@ test('Password to small', async (done) => {
     done();
 });
 
+test('Email invalid', async (done) => {
+    request = appTester.getRequestSender();
+    const query = {
+        query: `mutation{
+            register(fields: {
+                username:"${user.username}" 
+                email:"wrong.email.com" 
+                password:"${user.password}"
+                age:${user.age}
+                receiveNewsletter:${user.receiveNewsletter},
+                gender:${user.gender}
+                firstName:"${user.firstName}" 
+                lastName:"${user.lastName}"}){
+              notifications{
+                  type
+                  message
+              }
+            }}`
+    }
+    const res = await request.post('/graphql')
+        .set('Accept', 'application/json')
+        .set("Content-Type", "application/json")
+        .send(JSON.stringify(query))
+        .then(res => JSON.parse(res.text));
+    expect(res.errors[0].message.includes("This email address is not valid!")).toBeTruthy();
+    done();
+});
+
+test('Username contains unauthorized characters', async (done) => {
+    request = appTester.getRequestSender();
+    const query = {
+        query: `mutation{
+            register(fields: {
+                username:"Yo^^^^" 
+                email:"${user.email}" 
+                password:"${user.password}"
+                age:${user.age}
+                receiveNewsletter:${user.receiveNewsletter},
+                gender:${user.gender}
+                firstName:"${user.firstName}" 
+                lastName:"${user.lastName}"}){
+              notifications{
+                  type
+                  message
+              }
+            }}`
+    }
+    const res = await request.post('/graphql')
+        .set('Accept', 'application/json')
+        .set("Content-Type", "application/json")
+        .send(JSON.stringify(query))
+        .then(res => JSON.parse(res.text));
+    expect(res.errors[0].message.includes("A username may only contain letters, numbers, dashes, dots and underscores")).toBeTruthy();
+    done();
+});
+
+test('Username to small', async (done) => {
+    request = appTester.getRequestSender();
+    const query = {
+        query: `mutation{
+            register(fields: {
+                username:"Yo" 
+                email:"${user.email}" 
+                password:"${user.password}"
+                age:${user.age}
+                receiveNewsletter:${user.receiveNewsletter},
+                gender:${user.gender}
+                firstName:"${user.firstName}" 
+                lastName:"${user.lastName}"}){
+              notifications{
+                  type
+                  message
+              }
+            }}`
+    }
+    const res = await request.post('/graphql')
+        .set('Accept', 'application/json')
+        .set("Content-Type", "application/json")
+        .send(JSON.stringify(query))
+        .then(res => JSON.parse(res.text));
+    expect(res.errors[0].message.includes("The username must contains more than 4 characters!")).toBeTruthy();
+    done();
+});
+
 test('Register a correct user', async (done) => {
     request = appTester.getRequestSender();
     const query = {
@@ -118,7 +202,7 @@ test('Register a correct user', async (done) => {
     done();
 });
 
-test('Username already exists!', async (done) => {
+test('Username already exists', async (done) => {
     request = appTester.getRequestSender();
     const query = {
         query: `mutation{
@@ -146,7 +230,7 @@ test('Username already exists!', async (done) => {
     done();
 });
 
-test('Email already exists!', async (done) => {
+test('Email already exists', async (done) => {
     request = appTester.getRequestSender();
     const query = {
         query: `mutation{
