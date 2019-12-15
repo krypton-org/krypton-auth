70a90e8a8e22598f7fc6abd7c2b8e1b4d9b2cc3f
JohannC <johann.corcuff@gmail.com>
2019-12-15 16:46:11 +0100

    Adding package properties

--- test/integration/Register.test.js
+++ test/integration/Register.test.js
@@ -25,6 +25,28 @@ let user2 = {
     receiveNewsletter: true
 };
 
+let user3= {
+    username: "username3",
+    email: "test3@test.com",
+    password: "password3",
+    firstName: "firstname3",
+    lastName: "lastname3",
+    age: 24,
+    gender: "Mrs",
+    receiveNewsletter: true
+};
+
+let user4= {
+    username: "username4",
+    email: "test4@test.com",
+    password: "password4",
+    firstName: "firstname4",
+    lastName: "lastname4",
+    age: 24,
+    gender: "Mrs",
+    receiveNewsletter: true
+};
+
 beforeAll(async (done) => {
     appTester = new AppTester({
         dbConfig: {
@@ -164,48 +186,84 @@ test('Register a correct user', async (done) => {
 
 test('Username already exists', async (done) => {
     request = appTester.getRequestSender();
-    const query = {
+    const query1 = {
         query: `mutation{
             register(fields: {
-                username:"${user2.username}" 
-                email:"other.email@mail.com" 
-                password:"${user1.password}"
-                age:${user1.age}
-                receiveNewsletter:${user1.receiveNewsletter},
-                gender:${user1.gender}
-                firstName:"${user1.firstName}" 
-                lastName:"${user1.lastName}"}){
+                username:"${user4.username}" 
+                email:"${user4.email}" 
+                password:"${user4.password}"
+                age:${user4.age}
+                receiveNewsletter:${user4.receiveNewsletter},
+                gender:${user4.gender}
+                firstName:"${user4.firstName}" 
+                lastName:"${user4.lastName}"}){
               notifications{
                   type
                   message
               }
             }}`
     }
-    const res = await request.postGraphQL(query);
+    let res = await request.postGraphQL(query1);
+    const query2 = {
+        query: `mutation{
+            register(fields: {
+                username:"${user4.username}" 
+                email:"other.email@mail.com" 
+                password:"${user4.password}"
+                age:${user4.age}
+                receiveNewsletter:${user4.receiveNewsletter},
+                gender:${user4.gender}
+                firstName:"${user4.firstName}" 
+                lastName:"${user4.lastName}"}){
+              notifications{
+                  type
+                  message
+              }
+            }}`
+    }
+    res = await request.postGraphQL(query2);
     expect(res.errors[0].message).toBe("Username already exists");
     done();
 });
 
 test('Email already exists', async (done) => {
     request = appTester.getRequestSender();
-    const query = {
+    const query1 = {
         query: `mutation{
             register(fields: {
-                username:"other_username" 
-                email:"${user2.email}" 
-                password:"${user1.password}"
-                age:${user1.age}
-                receiveNewsletter:${user1.receiveNewsletter},
-                gender:${user1.gender}
-                firstName:"${user1.firstName}" 
-                lastName:"${user1.lastName}"}){
+                username:"${user3.username}" 
+                email:"${user3.email}" 
+                password:"${user3.password}"
+                age:${user3.age}
+                receiveNewsletter:${user3.receiveNewsletter},
+                gender:${user3.gender}
+                firstName:"${user3.firstName}" 
+                lastName:"${user3.lastName}"}){
               notifications{
                   type
                   message
               }
             }}`
     }
-    const res = await request.postGraphQL(query);
+    let res = await request.postGraphQL(query1);
+    const query2 = {
+        query: `mutation{
+            register(fields: {
+                username:"other_username" 
+                email:"${user3.email}" 
+                password:"${user3.password}"
+                age:${user3.age}
+                receiveNewsletter:${user3.receiveNewsletter},
+                gender:${user3.gender}
+                firstName:"${user3.firstName}" 
+                lastName:"${user3.lastName}"}){
+              notifications{
+                  type
+                  message
+              }
+            }}`
+    }
+    res = await request.postGraphQL(query2);
     expect(res.errors[0].message).toBe("Email already exists");
     done();
 });
