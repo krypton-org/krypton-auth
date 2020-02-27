d8b6a32a90d79b411e508568681786f3866ce149
jrebecchi <johann.corcuff@gmail.com>
2020-02-27 22:32:05 +0100

    Adding an example usage

---
+++ example/app.js
@@ -0,0 +1,12 @@
+/**
+ * DISCLAIMER: you need to run a local instance of MongoDB for this to work
+ */
+const { GraphQLAuthService } = require('graphql-auth-service');
+const express = require('express');
+const app = express();
+
+app.use('/auth', GraphQLAuthService()); //API entry point is localhost:5000/auth
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`server is listening on ${process.env.PORT || 5000}`)
+});
