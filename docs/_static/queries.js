66c1b1a02d68ca5763ba52edbcbeac3958e564fc
jrebecchi <johann.corcuff@gmail.com>
2020-03-19 18:31:51 +0100

    Bug fixed - Adding the auth token (in the request headers) only on queries needing it

--- docs/_static/queries.js
+++ docs/_static/queries.js
@@ -12,7 +12,7 @@ function setupGraphiQLHelpers(fetchURL, socketURL){
     });
 
     function getInputsInWhichToSetAuthToken(){
-        return document.querySelectorAll(".token-container input");
+        return document.querySelectorAll(".with-token .token-container input");
     }
 
     function pushToast(data) {
@@ -47,35 +47,37 @@ function setupGraphiQLHelpers(fetchURL, socketURL){
         token = newToken;
     }
 
-    function graphQLFetcher(graphQLParams) {
-        var headers = {
-            'Content-Type': 'application/json'
-        }
-        if (token) {
-            headers['Authorization'] = 'Bearer ' + token;
-        }
-        return fetch(fetchURL, {
-            method: 'post',
-            headers: headers,
-            credentials: 'include',
-            body: JSON.stringify(graphQLParams),
-        }).then(function(response) {
-            return response.json();
-        }).then(function(response) {
-            if (response &&
-                response.data &&
-                response.data.login &&
-                response.data.login.token) {
-                updateTokenInputs(response.data.login.token);
+    function getFetcher(isAuthRequired){
+        return function graphQLFetcher(graphQLParams) {
+            var headers = {
+                'Content-Type': 'application/json'
             }
-            if (response &&
-                response.data &&
-                response.data.refreshToken &&
-                response.data.refreshToken.token) {
-                updateTokenInputs(response.data.refreshToken.token);
+            if (token && isAuthRequired) {
+                headers['Authorization'] = 'Bearer ' + token;
             }
-            return response;
-        });;
+            return fetch(fetchURL, {
+                method: 'post',
+                headers: headers,
+                credentials: 'include',
+                body: JSON.stringify(graphQLParams),
+            }).then(function(response) {
+                return response.json();
+            }).then(function(response) {
+                if (response &&
+                    response.data &&
+                    response.data.login &&
+                    response.data.login.token) {
+                    updateTokenInputs(response.data.login.token);
+                }
+                if (response &&
+                    response.data &&
+                    response.data.refreshToken &&
+                    response.data.refreshToken.token) {
+                    updateTokenInputs(response.data.refreshToken.token);
+                }
+                return response;
+            });;
+        }
     }
 
     function setupGraphiQL() {
@@ -116,12 +118,13 @@ function setupGraphiQLHelpers(fetchURL, socketURL){
             var target = targets[i];
             var query = target.getElementsByClassName("query")[0].innerHTML.trim();
             var response = target.getElementsByClassName("response")[0].innerHTML.trim();
+            const isAuthRequired = target.classList.contains('with-token');
             var graphiQLElement = React.createElement(GraphiQLIDE, {
-                fetcher: graphQLFetcher,
+                fetcher: getFetcher(isAuthRequired),
                 onTokenUpdate: onTokenUpdate,
                 query: query,
                 response: response,
-                isAuthRequired: target.classList.contains('with-token'),
+                isAuthRequired,
             });
             ReactDOM.render(graphiQLElement, target);
         }
