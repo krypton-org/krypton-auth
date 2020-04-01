121e0df6daa9730f15eef5097354599f80a25d6b
jrebecchi <johann.corcuff@gmail.com>
2020-04-02 01:08:57 +0200

    Correct demo server config

--- jam/bon.ts
+++ jam/bon.ts
@@ -3,17 +3,7 @@ import express from 'express';
 
 const app = express();
 
-app.use('/auth', kryptonAuth({ nodemailerConfig: {
-    from: '"NUSID Demo" <nusid.demo@gmail.com>',
-    host: 'smtp.gmail.com', // hostname 
-    secureConnection: true, // use SSL 
-    port: 465, // port for secure SMTP 
-    transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer acce$
-    auth: {
-        user: 'nusid.demo@gmail.com',
-        pass: 'nusid@pp'
-    }
-}}));
+app.use('/auth', kryptonAuth());
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server is listening on ${process.env.PORT || 5000}`)
