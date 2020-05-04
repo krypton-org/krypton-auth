eb768d30bcb002e3f8e77c1cf15680ba5f8c5cfe
jrebecchi <johann.corcuff@gmail.com>
2020-05-05 01:34:42 +0200

    Safe merge or user properties with Krypton config

--- test/unit/ValidateUrl.test.ts
+++ test/unit/ValidateUrl.test.ts
@@ -1,52 +1,52 @@
 import config from '../../src/config';
 
 test("Transform mal-formed URL into well-formed URL",() => {
-    config.merge({});
+    config.set({});
     expect(config.getDomainAddress()).toBe(null);
     
-    config.merge({host: "example.com"});
+    config.set({host: "example.com"});
     expect(config.host).toBe("http://example.com");
     expect(config.hostURLObject.href).toBe("http://example.com");
     expect(config.hostURLObject.hostname).toBe("example.com");
     expect(config.getDomainAddress()).toBe("example.com");
 
-    config.merge({host: "://example.com"});
+    config.set({host: "://example.com"});
     expect(config.host).toBe("http://example.com");
     expect(config.hostURLObject.href).toBe("http://example.com");
     expect(config.hostURLObject.hostname).toBe("example.com");
     expect(config.getDomainAddress()).toBe("example.com");
 
-    config.merge({host: "example.com/"});
+    config.set({host: "example.com/"});
     expect(config.host).toBe("http://example.com");
     expect(config.hostURLObject.href).toBe("http://example.com");
     expect(config.hostURLObject.hostname).toBe("example.com");
     expect(config.getDomainAddress()).toBe("example.com");
 
-    config.merge({host: "example .com/"});
+    config.set({host: "example .com/"});
     expect(config.host).toBe("http://example.com");
     expect(config.hostURLObject.href).toBe("http://example.com");
     expect(config.hostURLObject.hostname).toBe("example.com");
     expect(config.getDomainAddress()).toBe("example.com");
 
-    config.merge({host: "www.example.com"});
+    config.set({host: "www.example.com"});
     expect(config.host).toBe("http://www.example.com");
     expect(config.hostURLObject.href).toBe("http://www.example.com");
     expect(config.hostURLObject.hostname).toBe("www.example.com");
     expect(config.getDomainAddress()).toBe("www.example.com");
 
-    config.merge({host: "://www.example.com"});
+    config.set({host: "://www.example.com"});
     expect(config.host).toBe("http://www.example.com");
     expect(config.hostURLObject.href).toBe("http://www.example.com");
     expect(config.hostURLObject.hostname).toBe("www.example.com");
     expect(config.getDomainAddress()).toBe("www.example.com");
 
-    config.merge({host: "www.example.com/"});
+    config.set({host: "www.example.com/"});
     expect(config.host).toBe("http://www.example.com");
     expect(config.hostURLObject.href).toBe("http://www.example.com");
     expect(config.hostURLObject.hostname).toBe("www.example.com");
     expect(config.getDomainAddress()).toBe("www.example.com");
 
-    config.merge({host: "www.example .com/"});
+    config.set({host: "www.example .com/"});
     expect(config.host).toBe("http://www.example.com");
     expect(config.hostURLObject.href).toBe("http://www.example.com");
     expect(config.hostURLObject.hostname).toBe("www.example.com");
